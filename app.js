2a45144c9273a073bc59ad9bfe54184240e7900a
orwel009 <orwelvincent1@gmail.com>
2024-06-11 15:37:21 +0530

    final commit

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require("express")
 const monogoos = require("mongoose")
 const cors = require("cors")
-// const students = require("./models/student")
+
+const employees = require("./models/employee")
+
+const {employeeModel} = require("./models/employee")
+
+
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("HELLO")
-})
+monogoos.connect("mongodb+srv://orwel000:orwel123@cluster0.hyugd.mongodb.net/employeeDB?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/add",(req,res)=>{
-    res.send("Add page")
+    let input = req.body
+    // console.log(input)
+    let employee =  new employeeModel(input)
+    // console.log(employee)
+    employee.save()
+    res.json({"status":"success"})
 })
 
-app.post("/view",(req,res)=>{
-    res.send("View Page")
+app.get("/view",(req,res)=>{
+    employeeModel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
 })
 
 app.listen(8080,()=>{
--- models/employee.js
+++ models/employee.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
-        "name":String,
-        "rollNo":String,
-        "admNo":String,
-        "college":String
+        "empId":{type:String,require:true},
+        "empName":{type:String,require:true},
+        "designation":{type:String,require:true},
+        "Salary":{type:String,require:true}
     }
 )
 
-let employeeModel = mongoose.Model("employees",schema)
+let employeeModel = mongoose.model("employees",schema)
 module.exports={employeeModel}
